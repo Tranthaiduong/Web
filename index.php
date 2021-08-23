3fa3bd25a39d8e9a5eb2dd8a79d4d07d1d4fe83b
Tranthaiduong <68003159+Tranthaiduong@users.noreply.github.com>
2021-08-24 06:55:49 +0700

    Index

---
+++ index.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/home.css">
+    <title>Trường Đại học Thủy lợi</title>
+</head>
+
+<body>
+    <!-- navbar: thanh điều hướng -->
+    <section class="Navbar">
+        <div class="container">
+            <div class="Logo">
+                <img src="img/logo.png" alt="School logo" class="img-reponsive">
+            </div>
+            
+            <div class="Menu text-right">
+                <ul>
+                    <li>
+                        <a href="#">Thông tin tuyển sinh</a>
+                    </li>
+                    <li>
+                        <a href="#">Chương trình đào tạo</a>
+                    </li>
+                    <li>
+                        <a href="#">Liên hệ</a>
+                    </li>
+                    <li>
+                        <a href="Login.php">Đăng nhập</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+    </section>
+
+    <!-- Search -->
+    <section class="Search text-center">
+        <div class="container">
+            <form action="">
+                <input type="search" name="search" placeholder="Search something...">
+                <input type="Submit" name="submit" value="Search" class="btn btn-primary">
+            </form>
+        </div>
+    </section>
+
+    
+
+    <!-- Cadres Menu -->
+    <section class="Cadres-Menu">
+        <div class="container">
+            <h2 class="text-center">Danh sách cán bộ Trường Đại học Thủy lợi</h2>
+
+            <div class="Cadres-Menu-Box">
+                <div class="Cadres-menu-img">
+                    <img src="img/Tung VIASM.jpg"class="img-reponsive img-curve">
+                </div>
+                <div class="Cadres-menu-desc">
+                    <h4>Nguyễn Thanh Tùng (PSG. TS.)</h4>
+                    <p class="food-price">Trưởng Khoa CNTT</p>
+                    <p class="food-detail">
+                        tungnt@tlu.edu.vn
+                    </p>
+                    <br>
+
+                    <a href="#" class="btn btn-primary">Xem thêm</a>
+                </div>
+
+                <div class="clearfix"></div>
+            </div>
+
+            <div class="Cadres-Menu-Box">
+                <div class="Cadres-menu-img">
+                    <img src="img/DangThuHien.jpg" class="img-reponsive img-curve">
+                </div>
+                <div class="Cadres-menu-desc">
+                    <h4>Đặng Thị Thu Hiền (TS.)</h4>
+                    <p class="food-price">Phó Trưởng Khoa CNT</p>
+                    <p class="food-detail">
+                        Hiendt@tlu.edu.vn
+                    </p>
+                    <br>
+
+                    <a href="#" class="btn btn-primary">Xem thêm</a>
+                </div>
+
+                <div class="clearfix"></div>
+            </div>
+
+            <div class="Cadres-Menu-Box">
+                <div class="Cadres-menu-img">
+                    <img src="img/Kieu Tuan Dung.jpg"class="img-reponsive img-curve">
+                </div>
+                <div class="Cadres-menu-desc">
+                    <h4>Kiều Tuấn Dũng (ThS)</h4>
+                    <p class="food-price">Giảng viên</p>
+                    <p class="food-detail">
+                        dungkt@tlu.edu.vn
+                    </p>
+                    <br>
+                    <a href="#" class="btn btn-primary">Xem thêm</a>
+                </div>
+
+                <div class="clearfix"></div>
+            </div>
+            <div class="Cadres-Menu-Box">
+                <div class="Cadres-menu-img">
+                    <img src="img/NguyenQuynhDiep.png"class="img-reponsive img-curve">
+                </div>
+                <div class="Cadres-menu-desc">
+                    <h4>Nguyễn Quỳnh Diệp (TS.)</h4>
+                    <p class="food-price">Trưởng Bộ môn</p>
+                    <p class="food-detail">
+                        diepnq@tlu.edu.vn
+                    </p>
+                    <br>
+
+                    <a href="#" class="btn btn-primary">Xem thêm</a>
+                </div>
+
+                <div class="clearfix"></div>
+            </div>
+
+            <div class="Cadres-Menu-Box">
+                <div class="Cadres-menu-img">
+                    <img src="img/nguyen thi ly.jpg"class="img-reponsive img-curve">
+                </div>
+                <div class="Cadres-menu-desc">
+                    <h4>Nguyễn Thị Lý (ThS)</h4>
+                    <p class="food-price">Giảng viên</p>
+                    <p class="food-detail">
+                        Lycs2@tlu.edu.vn
+                    </p>
+                    <br>
+
+                    <a href="#" class="btn btn-primary">Xem thêm</a>
+                </div>
+
+                <div class="clearfix"></div>
+            </div>
+
+            <div class="Cadres-Menu-Box">
+                <div class="Cadres-menu-img">
+                    <img src="img/Do Oanh Cuong 2.jpg"class="img-reponsive img-curve">
+                </div>
+                <div class="Cadres-menu-desc">
+                    <h4>Đỗ Oanh Cường (ThS)</h4>
+                    <p class="food-price">Giảng viên</p>
+                    <p class="food-detail">
+                        Cuongdo@tlu.edu.vn
+                    </p>
+                    <br>
+
+                    <a href="#" class="btn btn-primary">Xem thêm</a>
+                </div>
+
+                <div class="clearfix"></div>
+            </div>
+
+        <div class="container text-center">
+            <form action="">
+                <input type="Submit" name="submit" value="Xem thêm" class="btn btn-primary">
+            </form>
+        </div>
+
+
+            
+
+            <div class="clearfix"></div>
+        </div>
+    </section>
+
+    <!-- Social -->
+    <section class="Social">
+        <div class="container text-center">
+            <ul>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/color/48/000000/facebook-circled--v1.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluency/48/000000/instagram-new.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/color/48/000000/twitter--v1.png"/></a>
+                </li>
+            </ul>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <section class="Footer">
+        <div class="container text-center">
+            <p>All right reserved. Design by <a href="#">Tran Thai Duong</a></p>
+        </div>
+    </section>
+
+</body>
+
+</html>
